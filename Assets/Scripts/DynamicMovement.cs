1da877e7b3de7d15dd349051e2bdad3c7432f606
CJ Legato <legatc@rpi.edu>
2018-09-18 11:41:49 -0400

    first push

---
+++ Assets/Scripts/DynamicMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicMovement : MonoBehaviour {
+
+
+    public enum Algorithm
+    {
+        Wander,
+        Pursue,
+        Evade,
+        Path
+    }
+
+    public Algorithm algorithm = Algorithm.Wander;
+
+    Vector3 linearVelocity;
+    float angularVelocity;
+    Vector3 linearAcceleration;
+    float angularAcceleration;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(linearVelocity * Time.deltaTime);
+        transform.Rotate(0f, 0f, angularVelocity);
+
+        linearVelocity += linearAcceleration * Time.deltaTime;
+        angularVelocity += angularAcceleration * Time.deltaTime;
+	}
+}
